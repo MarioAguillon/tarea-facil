a19b865a7ae07caf2a104069516f4ed98f925d18
Laura <lauradaniela200707@gmail.com>
2026-02-26 16:01:06 -0500

    tarea-facil

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Para que funcionen cosas básicas
+// Importamos tus 3 componentes:
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { MainContent } from './components/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true, // Esto confirma que es modo moderno
+  imports: [
+    CommonModule, 
+    Header,   // <--- Agrégalo aquí
+    Sidebar,  // <--- Agrégalo aquí
+    MainContent // <--- Agrégalo aquí
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('tarea-facil');
+export class AppComponent {
+  title = 'tarea-facil';
 }
